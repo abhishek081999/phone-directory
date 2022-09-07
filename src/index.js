4e8b1a6b0e26f880d185c737adaf0f9e643fca52
Abhishek Kumar <abhishekbhoranj@gmail.com>
2022-09-07 10:44:46 +0530

    Added routing for Add Subscriber page and Show Subscribers page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import ShowSubscribers from './ShowSubscribers';
+// import ShowSubscribers from './ShowSubscribers';
 import reportWebVitals from './reportWebVitals';
 // import AddSubscriber from "./AddSubscriber";
 import './common/common.css'
-import AddSubscriber from './AddSubscriber';
+// import AddSubscriber from './AddSubscriber';
 import PhoneDirectory from './PhoneDirectory';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PhoneDirectory />
+    <PhoneDirectory/>
   </React.StrictMode>
 );
 
