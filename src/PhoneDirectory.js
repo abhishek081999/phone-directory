080f633449f4ed43f779e46be1c9c7f519e7ed7d
Abhishek Kumar <abhishekbhoranj@gmail.com>
2022-09-09 12:17:45 +0530

    usecontext hook  footer

--- src/PhoneDirectory.js
+++ src/PhoneDirectory.js
@@ -1,62 +1,82 @@
-import React from 'react';
-import AddSubscriber from './AddSubscriber';
-import ShowSubscribers from './ShowSubscribers';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { useState } from 'react';
+import React, { Fragment } from "react";
+import AddSubscriber from "./AddSubscriber";
+import ShowSubscribers from "./ShowSubscribers";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { useState } from "react";
+import Footer from "./Footer"
+import { SubscriberCountContext } from "./SubscriberCountContext";
 export default function PhoneDirectory() {
-        const [subscribersList, setSubscribersList] = useState( [
-                             {
-                                 id: 1,
-                                 name: "Abhishek",
-                                 phone: "9999999999"
-                             },
-                             {
-                                 id: 2,
-                                 name: "Ganesh",
-                                 phone: "8888888888"
-                             }
-                         ]);
+  const [subscribersList, setSubscribersList] = useState([
+    {
+      id: 1,
+      name: "Abhishek",
+      phone: "9999999999",
+    },
+    {
+      id: 2,
+      name: "Ganesh",
+      phone: "8888888888",
+    },
+  ]);
 
-                        function  deleteSubscriberHandler (subscriberId)  {
-                                     
-                                     const newSubscribers = subscribersList.filter(
-                                        (subscriber) => subscriber.id !== subscriberId);
-                                     
-                                     setSubscribersList(newSubscribers);
-                                 }
+  function deleteSubscriberHandler(subscriberId) {
+    const newSubscribers = subscribersList.filter(
+      (subscriber) => subscriber.id !== subscriberId
+    );
 
+    setSubscribersList(newSubscribers);
+  }
 
-                                function addSubscriberHandler (newSubscriber) {
-                                             
-                                             if (subscribersList.length > 0) {
-                                                 newSubscriber.id = subscribersList[subscribersList.length - 1].id + 1;
-                                             } else {
-                                                newSubscriber.id = 1;
-                                             }
-                                             subscribersList.push(newSubscriber);
-                                             setSubscribersList(subscribersList);
-                                            //  this.setState({ subscribersList: subscribersList });
-                                         }
-                                    
+  function addSubscriberHandler(newSubscriber) {
+    if (subscribersList.length > 0) {
+      newSubscriber.id = subscribersList[subscribersList.length - 1].id + 1;
+    } else {
+      newSubscriber.id = 1;
+    }
+    subscribersList.push(newSubscriber);
+    setSubscribersList(subscribersList);
+    //  this.setState({ subscribersList: subscribersList });
+  }
 
-    return (
-                    <Router>
-                        <div>
-                            <Route exact path="/" render={(props) => <ShowSubscribers {...props} subscribersList={subscribersList}deleteSubscriberHandler={(subscriberId)=>deleteSubscriberHandler(subscriberId)} />} />
-                            <Route exact path="/add" render={({history}, props) => <AddSubscriber history={history} {...props} addSubscriberHandler={(newSubscriber)=>addSubscriberHandler(newSubscriber)} />} />
-                        </div>
-                    </Router>
-                ) 
+  return (
+    <Fragment>
+      <Router>
+        <div>
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <ShowSubscribers
+                {...props}
+                subscribersList={subscribersList}
+                deleteSubscriberHandler={(subscriberId) =>
+                  deleteSubscriberHandler(subscriberId)
+                }
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/add"
+            render={({ history }, props) => (
+              <AddSubscriber
+                history={history}
+                {...props}
+                addSubscriberHandler={(newSubscriber) =>
+                  addSubscriberHandler(newSubscriber)
+                }
+              />
+            )}
+          />
+        </div>
+      </Router>
+      <SubscriberCountContext.Provider value={subscribersList.length}>
+      <Footer ></Footer>
+      </SubscriberCountContext.Provider>
+      
+    </Fragment>
+  );
 }
-
-
-
-
-
-
-
-
-
 
 // class PhoneDirectory extends Component {
 
@@ -90,7 +110,6 @@ export default function PhoneDirectory() {
 //         newSubscribers.splice(subscriberIndex, 1);
 //         this.setState({subscribers: newSubscribers});
 //     }
-
 
 //     addSubscriberHandler = (newSubscriber) => {
 //         let subscribersList = this.state.subscribersList;
